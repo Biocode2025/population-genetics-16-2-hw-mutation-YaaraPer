688b62110ab64d7d5003b88f8719894fd4c69c67
YaaraPer <yaaraperlmuter@gmail.com>
2026-02-23 19:49:47 +0200

    חישוב התפלגויות הגנוטיפים ושכיחות האלל q.

--- scripts/code.py
+++ scripts/code.py
@@ -1,8 +1,18 @@
 # הגדרת משתנים
 q = 0.02
-c = 0
-gen = 10
+gen = 500
 
 # פתיחת קובץ 
 f = open('results/CF_freq.txt', 'w')
 f.write("Generation\tFreq_c\tFreq_CC\tFreq_Cc\tFreq_cc\n")
+
+# הגדרת לולאת דורות
+for i in range(1, gen + 1):
+    # חישוב ההתפלגויות של שכיחות האלל q והתפלגות הגנוטיפים CC, Cc, cc.
+    p = 1 - q
+    CC = p**2
+    Cc = 2*q*p
+    cc = q**2
+    q = q / (q + 1)
+
+    
